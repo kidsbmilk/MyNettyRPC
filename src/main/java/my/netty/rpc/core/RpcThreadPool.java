b9f5e0110769bc2eb8b72c1e9f6be2358d897d61
zhangzhen5 <zhangzhen5@xiaomi.com>
2018-07-20 20:56:04 +0800

    add files on v2

--- src/main/java/my/netty/rpc/core/RpcThreadPool.java
+++ src/main/java/my/netty/rpc/core/RpcThreadPool.java
@@ -6,6 +6,10 @@ import java.util.concurrent.*;
 
 public class RpcThreadPool {
 
+    private static RejectedExecutionHandler createPolicy() {
+
+    }
+
     public static Executor getExecutor(int threads, int queues) {
         String name = "RpcThreadPool";
         return new ThreadPoolExecutor(threads, threads, 0, TimeUnit.MILLISECONDS,
