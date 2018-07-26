a140f9a7464bfdf2ee1c86547fe46046d2613dd3
zhangzhen5 <zhangzhen5@xiaomi.com>
2018-07-26 19:40:17 +0800

    Reflection.newProxy

--- src/test/java/my/netty/rpc/test/PojoCallTest.java
+++ src/test/java/my/netty/rpc/test/PojoCallTest.java
@@ -18,6 +18,20 @@ public class PojoCallTest {
 
         int result = manage.save(p); // 这里才会调用代理对象MessageSendProxy的handleInvocation方法，进行远程过程调用，整个过程，对客户端来说，就像是在本地调用的一样，感觉很神奇。
         // 疑问：manage.save经过远程过程调用、反序列化啥的，返回的应该是Object，为什么可以直接赋给result呢？是在哪里做了类型转换吗？
+        // google Guava包的reflection解析
+        // https://yq.aliyun.com/articles/20560
+        /**
+         * JDK动态代理需要类型转换：
+         * Foo foo = (Foo) Proxy.newProxyInstance(
+         * Foo.class.getClassLoader(),
+         * new Class<?>[] {Foo.class},
+         * invocationHandler);
+         *
+         * Guava不需要类型转换：
+         * Foo foo = Reflection.newProxy(Foo.class, invocationHandler);
+         *
+         * 但是，内部原理还是不太明白？
+         */
 
         Person p2 = manage.getPerson(p);
         System.out.println(p2);
