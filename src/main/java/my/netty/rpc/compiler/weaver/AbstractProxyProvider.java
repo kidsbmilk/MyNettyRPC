ed6433e572b70a971e744fc0bed235b01861d3e5
zhangzhen5 <zhangzhen5@xiaomi.com>
2018-08-06 10:09:39 +0800

    refactoring class structure

--- src/main/java/my/netty/rpc/compiler/weaver/AbstractProxyProvider.java
+++ src/main/java/my/netty/rpc/compiler/weaver/AbstractProxyProvider.java
@@ -1,6 +1,8 @@
 package my.netty.rpc.compiler.weaver;
 
 import my.netty.rpc.compiler.intercept.Interceptor;
+import my.netty.rpc.compiler.invoke.InterceptorInvoker;
+import my.netty.rpc.compiler.invoke.ObjectInvoker;
 
 public abstract class AbstractProxyProvider implements ClassProxy {
 
@@ -8,4 +10,11 @@ public abstract class AbstractProxyProvider implements ClassProxy {
     public <T> T createProxy(Object target, Interceptor interceptor, Class<?>... proxyClasses) {
         return createProxy(Thread.currentThread().getContextClassLoader(), target, interceptor, proxyClasses);
     }
+
+    @Override
+    public <T> T createProxy(ClassLoader classLoader, Object target, Interceptor interceptor, Class<?>... proxyClasses) { // 这个是接口ClassProxy里的另一个方法
+        return createProxyProvider(classLoader, new InterceptorInvoker(target, interceptor), proxyClasses);
+    }
+
+    abstract <T> T createProxyProvider(ClassLoader classLoader, ObjectInvoker interceptorInvoker, Class<?>... proxyClasses); // 创建自己的抽象方法，使类继承以及接口实现层次更清晰。
 }
--- src/main/java/my/netty/rpc/compiler/weaver/ProxyProvider.java
+++ src/main/java/my/netty/rpc/compiler/weaver/ProxyProvider.java
@@ -9,11 +9,7 @@ public class ProxyProvider extends AbstractProxyProvider {
     private static final ClassCache PROXY_CLASS_CACHE = new ClassCache(new ByteCodeClassTransformer());
 
     @Override
-    public <T> T createProxy(ClassLoader classLoader, Object target, Interceptor interceptor, Class<?>... proxyClasses) {
-        return createProxyHere(classLoader, new InterceptorInvoker(target, interceptor), proxyClasses);
-    }
-
-    private <T> T createProxyHere(ClassLoader classLoader, ObjectInvoker invoker, final Class<?>... proxyClasses) {
+    <T> T createProxyProvider(ClassLoader classLoader, ObjectInvoker invoker, final Class<?>... proxyClasses) {
         Class<?> proxyClass = PROXY_CLASS_CACHE.getProxyClass(classLoader, proxyClasses);
         try {
             T result = (T) proxyClass.getConstructor(ObjectInvoker.class).newInstance(invoker);
