daa2fc7739cdda912a2e47ec011f34f15912d52f
zhangzhen5 <zhangzhen5@xiaomi.com>
2018-08-06 09:53:32 +0800

    make function name distinguishable

--- src/main/java/my/netty/rpc/compiler/weaver/ProxyProvider.java
+++ src/main/java/my/netty/rpc/compiler/weaver/ProxyProvider.java
@@ -10,10 +10,10 @@ public class ProxyProvider extends AbstractProxyProvider {
 
     @Override
     public <T> T createProxy(ClassLoader classLoader, Object target, Interceptor interceptor, Class<?>... proxyClasses) {
-        return createProxy(classLoader, new InterceptorInvoker(target, interceptor), proxyClasses);
+        return createProxyHere(classLoader, new InterceptorInvoker(target, interceptor), proxyClasses);
     }
 
-    private <T> T createProxy(ClassLoader classLoader, ObjectInvoker invoker, final Class<?>... proxyClasses) {
+    private <T> T createProxyHere(ClassLoader classLoader, ObjectInvoker invoker, final Class<?>... proxyClasses) {
         Class<?> proxyClass = PROXY_CLASS_CACHE.getProxyClass(classLoader, proxyClasses);
         try {
             T result = (T) proxyClass.getConstructor(ObjectInvoker.class).newInstance(invoker);
